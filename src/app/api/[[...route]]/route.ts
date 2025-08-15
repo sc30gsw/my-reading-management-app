e09214c0a14694b232f27ffd1f62a7d8096ec507
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-08-15 15:39:11 +0900

    ルートハンドラーを修正し、タスクAPIのサンプルをコメントアウト。認証ルートを新規作成。

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,8 +2,6 @@ import { OpenAPIHono } from '@hono/zod-openapi'
 import { Scalar } from '@scalar/hono-api-reference'
 import { handle } from 'hono/vercel'
 
-// import { taskApi } from '~/features/tasks/api/route'
-
 const app = new OpenAPIHono().basePath('/api')
 
 app
@@ -16,8 +14,13 @@ app
   })
   .get('/scalar', Scalar({ url: '/api/doc' }))
 
+//  Here's a sample
 // const route = app.route('/tasks', taskApi)
-const route = app.route('/tasks')
-export type AppType = typeof route
+// const route = app.route('/tasks')
+export type AppType = typeof app
 
-export const GET = handle(route)
+export const GET = handle(app)
+export const POST = handle(app)
+export const PUT = handle(app)
+export const DELETE = handle(app)
+export const PATCH = handle(app)
