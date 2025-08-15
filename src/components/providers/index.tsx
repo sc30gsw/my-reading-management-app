73d7d4375474a5e19e1c80e0275ed46087715c01
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-08-15 15:38:04 +0900

    コンポーネントの追加と既存コンポーネントの修正、トーストの色定数を追加

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,6 +1,17 @@
-import type { ReactNode } from 'react'
+'use client'
+import { type ReactNode, useEffect, useState } from 'react'
 import { NuqsProvider } from '~/components/providers/nuqs-provider'
 
 export function Providers({ children }: Record<'children', ReactNode>) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
   return <NuqsProvider>{children}</NuqsProvider>
 }
